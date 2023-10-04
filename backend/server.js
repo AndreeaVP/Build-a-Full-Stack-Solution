27f2329b73c2d1cf53b958ebd34a58d21a89d121
daisy2511 <andreeagatu58@yahoo.com>
2023-10-04 11:02:48 +0100

    database connection created

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,46 @@
 const http = require('http');
 const app = require('./app');
+const dotenv = require('dotenv');
+const database = require('./config/database');
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+dotenv.config();
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
-const errorHandler = error => {
+database.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+const server = http.createServer(app);
+
+server.on('error', (error) => {
   if (error.syscall !== 'listen') {
     throw error;
   }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
       throw error;
   }
-};
+});
 
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   console.log('Listening on ' + bind);
 });
 
