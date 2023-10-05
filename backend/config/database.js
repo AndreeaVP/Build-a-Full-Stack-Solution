c6667f3f5d06c8619d38fdba85ca25c7342ec922
daisy2511 <andreeagatu58@yahoo.com>
2023-10-06 00:05:11 +0100

    database update from mysql to mysql2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,9 @@
-const mysql = require('mysql');
+const mysql2 = require('mysql2');
 
-module.exports = mysql.createConnection({
+module.exports = mysql2.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: 'utf8mb4',
 });
