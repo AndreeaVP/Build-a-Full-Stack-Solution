b703f27aaedf63334e592e554d08542c93225cbb
daisy2511 <andreeagatu58@yahoo.com>
2023-10-06 00:01:40 +0100

    user routes imported

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const path = require('path');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/posts');
+const likeRoutes = require('./routes/likes');
+const commentRoutes = require('./routes/comments');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(helmet());  
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); 
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+module.exports = app;  
