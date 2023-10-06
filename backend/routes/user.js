93539d5ec2ed801cfc765170a7bd36c8c77cf533
daisy2511 <andreeagatu58@yahoo.com>
2023-10-06 20:43:48 +0100

    replaced userController with userCtrl

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user');
+const userCtrl = require('../controllers/user');
 const multer = require('../middleware/multer');
 const auth = require('../middleware/auth');
 
-router.get('/', auth, userController.getAllUsers);
-router.get('/:id', auth, userController.getUserById);
-router.put('/:id', auth, multer.single('image_url'), userController.updateUser);
-router.delete('/:id', auth,  userController.deleteUser);
+router.get('/', auth, userCtrl.getAllUsers);
+router.get('/:id', auth, userCtrl.getUserById);
+router.put('/:id', auth, multer.single('image_url'), userCtrl.updateUser);
+router.delete('/:id', auth,  userCtrl.deleteUser);
 
 module.exports = router;
