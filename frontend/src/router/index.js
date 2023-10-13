93276e408d7870db3bcc42e475e1a82297c75944
daisy2511 <andreeagatu58@yahoo.com>
2023-10-14 00:44:55 +0100

    homepage routes declared

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import AuthenticationPage from '../views/authentication-page.vue';
+import HomePage from '../views/home-page.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'login',
     component: AuthenticationPage,
+  },
+
+  {
+    path: '/home',
+    name: 'home', 
+    component: HomePage,
   },
 ];
 
