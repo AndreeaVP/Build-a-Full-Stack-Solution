1f083856cf088e294e7c213b63be1e4ec8b3b9d4
daisy2511 <andreeagatu58@yahoo.com>
2023-10-14 00:51:55 +0100

    user authentication and API requests

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,54 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
-  },
-  getters: {
+    token: null, 
+    user: null,
   },
   mutations: {
+    setToken(state, token) {
+      state.token = token;
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
+    async signup({ commit }, userData) {
+      try {
+        const response = await axios.post('/api/auth/signup', userData);
+  
+        if (response.data.token) {
+          commit('setToken', response.data.token);
+          commit('setUser', response.data.user);
+          return response;
+        } else {
+          return Promise.reject('Invalid response');
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
+    async login({ commit }, credentials) {
+      try {
+        const response = await axios.post('/api/auth/login', credentials);
+  
+        if (response.data.token) {
+          commit('setToken', response.data.token);
+          commit('setUser', response.data.user);
+          return response;
+        } else {
+          return Promise.reject('Invalid response');
+        }
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  
+  getters: {
+    getToken: (state) => state.token,
+    getUser: (state) => state.user,
+  }  
+});
