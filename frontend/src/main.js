01220a91722d340663a1c1a9b128e04c1065a328
daisy2511 <andreeagatu58@yahoo.com>
2023-10-14 23:08:00 +0100

    font awesome component imported and created

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
+
