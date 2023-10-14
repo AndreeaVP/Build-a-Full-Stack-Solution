47b37d2cf664f0804fcc5d4ae52bbd28a6768a99
daisy2511 <andreeagatu58@yahoo.com>
2023-10-14 23:27:31 +0100

    header imported on the home page

---
+++ frontend/src/views/home-page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/app-header.vue";
+
+export default {
+  components: {
+    AppHeader, 
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
