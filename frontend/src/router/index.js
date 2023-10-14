0ec5723c56f43046f642b6fb44e722ad05d9882e
daisy2511 <andreeagatu58@yahoo.com>
2023-10-14 23:31:35 +0100

    routes created for notification, user profile, search page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import AuthenticationPage from '../views/authentication-page.vue';
 import HomePage from '../views/home-page.vue';
+import SearchPage from '../views/search-page.vue';
+import NotificationPage from '../views/notification-page.vue';
+import UserProfilePage from '../views/userprofile-page.vue';
 
 const routes = [
   {
@@ -8,11 +11,25 @@ const routes = [
     name: 'login',
     component: AuthenticationPage,
   },
-
   {
     path: '/home',
-    name: 'home', 
+    name: 'home',
     component: HomePage,
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchPage, 
+  },
+  {
+    path: '/notification',
+    name: 'notification',
+    component: NotificationPage, 
+  },
+  {
+    path: '/userprofile',
+    name: 'userprofile',
+    component: UserProfilePage, 
   },
 ];
 
