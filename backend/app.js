d5ab2299fed72a57feaa00e254f4ac1748481dba
daisy2511 <andreeagatu58@yahoo.com>
2023-10-28 22:43:05 +0100

    cors moved on top of routes

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,13 @@ const commentRoutes = require('./routes/comments');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.simple(),
@@ -29,21 +36,11 @@ const morganStream = {
 };
 
 app.use(morgan('combined', { stream: morganStream }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-
 app.use(helmet());  
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/posts', postRoutes);
