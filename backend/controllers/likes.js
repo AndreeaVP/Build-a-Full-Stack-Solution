4b1b0df1ea379f2a3eb12b16aa1810a65b69f430
daisy2511 <andreeagatu58@yahoo.com>
2023-11-12 23:09:24 +0000

    create likes/dislikes and get likes controller code changed, and working as expected

--- backend/controllers/likes.js
+++ backend/controllers/likes.js
@@ -1,132 +1,58 @@
 const db = require('../config/database');
 
-
 exports.createUpdateDeleteLike = (req, res) => {
   const { like, dislike, post_id, user_id } = req.body;
 
-  if (like === 1) {
-    // Check if the user has already liked the post
-    db.query(
-      'SELECT likes FROM likes WHERE post_id = ? AND user_id_liked = ?',
-      [post_id, user_id],
-      (err, result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        if (result.length > 0) {
-          // Update the likes column for the existing record
-          db.query(
-            'UPDATE likes SET likes = ? WHERE post_id = ? AND user_id_liked = ?',
-            [like, post_id, user_id],
-            (err, updateResult) => {
-              if (err) {
-                return res.status(500).send(err);
-              }
-              res.status(200).json({ message: "Like updated!" });
-            }
-          );
-        } else {
-          // Insert a new like record
-          db.query(
-            'INSERT INTO likes (likes, post_id, user_id_liked) VALUES (?, ?, ?)',
-            [like, post_id, user_id],
-            (err, insertResult) => {
-              if (err) {
-                return res.status(500).send(err);
-              }
-              res.status(201).json({ message: "Like added!" });
-            }
-          );
-        }
-      }
-    );
-  } else if (like === 0) {
-    // Remove the like
-    db.query(
-      'UPDATE likes SET likes = ? WHERE post_id = ? AND user_id_liked = ?',
-      [like, null, post_id, user_id],
-      (err, result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        res.status(200).json({ message: "Like removed!" });
-      }
-    );
-  }
-
-  if (dislike === 1) {
-    // Check if the user has already disliked the post
-    db.query(
-      'SELECT dislikes FROM likes WHERE post_id = ? AND user_id_disliked = ?',
-      [post_id, user_id],
-      (err, result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        if (result.length > 0) {
-          // Update the dislikes column for the existing record
-          db.query(
-            'UPDATE likes SET dislikes = ? WHERE post_id = ? AND user_id_disliked = ?',
-            [dislike, post_id, user_id],
-            (err, updateResult) => {
-              if (err) {
-                return res.status(500).send(err);
-              }
-              res.status(200).json({ message: "Dislike updated!" });
-            }
-          );
-        } else {
-          // Insert a new dislike record
-          db.query(
-            'INSERT INTO likes (dislikes, post_id, user_id_disliked) VALUES (?, ?, ?)',
-            [dislike, post_id, user_id],
-            (err, insertResult) => {
-              if (err) {
-                return res.status(500).send(err);
-              }
-              res.status(201).json({ message: "Dislike added!" });
-            }
-          );
-        }
-      }
-    );
-  } else if (dislike === 0) {
-    // Remove the dislike
-    db.query(
-      'UPDATE likes SET dislikes = ? WHERE post_id = ? AND user_id_disliked = ?',
-      [dislike, null, post_id, user_id],
-      (err, result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        res.status(200).json({ message: "Dislike removed!" });
-      }
-    );
-  }
-};
-
-exports.getLikesByPostId = (req, res) => {
-  const postId = req.params.postId;
-
-  db.query(
-    'SELECT * FROM likes WHERE post_id = ?',
-    [postId],
-    (err, results) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.status(200).json({ likes: results });
+  db.query('SELECT * FROM likes WHERE post_id = ? AND (user_id_liked = ? OR user_id_disliked = ?)', [post_id, user_id, user_id], (err, existingLike) => {
+    if (err) {
+      console.error('Error querying existing like:', err);
+      return res.status(500).json({ message: 'Internal Server Error.' });
     }
-  );
+
+    try {
+      if (existingLike.length > 0) {
+        if (like === 1) {
+          db.query('DELETE FROM likes WHERE post_id = ? AND user_id_liked = ? AND user_id_disliked IS NULL AND likes = 1', [post_id, user_id], (err) => {
+            if (err) {
+              console.error('Error canceling like:', err);
+              return res.status(500).json({ message: 'Internal Server Error.' });
+            }
+            res.status(200).json({ message: 'Like removed successfully.' });
+          });
+        } else if (dislike === 1) {
+          db.query('DELETE FROM likes WHERE post_id = ? AND user_id_liked IS NULL AND user_id_disliked = ? AND dislikes = 1', [post_id, user_id], (err) => {
+            if (err) {
+              console.error('Error canceling dislike:', err);
+              return res.status(500).json({ message: 'Internal Server Error.' });
+            }
+            res.status(200).json({ message: 'Dislike removed successfully.' });
+          });
+        } 
+      } else {
+        if ((like === 1 && dislike === 0) || (like === 0 && dislike === 1)) {
+          db.query('INSERT INTO likes (likes, dislikes, post_id, user_id_liked, user_id_disliked) VALUES (?, ?, ?, ?, ?)', [like, dislike, post_id, like === 1 ? user_id : null, dislike === 1 ? user_id : null], (err) => {
+            if (err) {
+              console.error('Error creating like or dislike:', err);
+              return res.status(500).json({ message: 'Internal Server Error.' });
+            }
+            res.status(200).json({ message: like === 1 ? 'Like added successfully.' : 'Dislike added successfully.' });
+          });
+        } else {
+          res.status(400).json({ message: 'Invalid request.' });
+        }
+      }
+    } catch (error) {
+      console.error('Error processing like or dislike:', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
+    }
+  });
 };
 
 exports.getLikesAndDislikes = (req, res) => {
-  db.query(`SELECT post_id, likes, dislikes, user_id_liked, user_id_disliked FROM likes`,
-    (err, result) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      res.status(200).send(result);
+  db.query(`SELECT post_id, likes, dislikes, user_id_liked, user_id_disliked FROM likes`, (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
     }
-  );
+    res.status(200).send(result);
+  });
 };
