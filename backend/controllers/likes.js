46c96b1dc69be911827070bfce0bbed4f12ca884
daisy2511 <andreeagatu58@yahoo.com>
2023-11-14 23:03:04 +0000

    dislike functionality removed / like functionality works as expected / get likes by postId code implemented

--- backend/controllers/likes.js
+++ backend/controllers/likes.js
@@ -1,9 +1,9 @@
 const db = require('../config/database');
 
-exports.createUpdateDeleteLike = (req, res) => {
-  const { like, dislike, post_id, user_id } = req.body;
+exports.createLike = (req, res) => {
+  const { post_id, user_id } = req.body;
 
-  db.query('SELECT * FROM likes WHERE post_id = ? AND (user_id_liked = ? OR user_id_disliked = ?)', [post_id, user_id, user_id], (err, existingLike) => {
+  db.query('SELECT * FROM likes WHERE post_id = ? AND user_id = ?', [post_id, user_id], (err, existingLike) => {
     if (err) {
       console.error('Error querying existing like:', err);
       return res.status(500).json({ message: 'Internal Server Error.' });
@@ -11,45 +11,33 @@ exports.createUpdateDeleteLike = (req, res) => {
 
     try {
       if (existingLike.length > 0) {
-        if (like === 1) {
-          db.query('DELETE FROM likes WHERE post_id = ? AND user_id_liked = ? AND user_id_disliked IS NULL AND likes = 1', [post_id, user_id], (err) => {
-            if (err) {
-              console.error('Error canceling like:', err);
-              return res.status(500).json({ message: 'Internal Server Error.' });
-            }
-            res.status(200).json({ message: 'Like removed successfully.' });
-          });
-        } else if (dislike === 1) {
-          db.query('DELETE FROM likes WHERE post_id = ? AND user_id_liked IS NULL AND user_id_disliked = ? AND dislikes = 1', [post_id, user_id], (err) => {
-            if (err) {
-              console.error('Error canceling dislike:', err);
-              return res.status(500).json({ message: 'Internal Server Error.' });
-            }
-            res.status(200).json({ message: 'Dislike removed successfully.' });
-          });
-        } 
+        db.query('DELETE FROM likes WHERE post_id = ? AND user_id = ?', [post_id, user_id], (err) => {
+          if (err) {
+            console.error('Error canceling like:', err);
+            return res.status(500).json({ message: 'Internal Server Error.' });
+          }
+          res.status(200).json({ message: 'Like removed successfully.', action: 'remove' });
+        });
       } else {
-        if ((like === 1 && dislike === 0) || (like === 0 && dislike === 1)) {
-          db.query('INSERT INTO likes (likes, dislikes, post_id, user_id_liked, user_id_disliked) VALUES (?, ?, ?, ?, ?)', [like, dislike, post_id, like === 1 ? user_id : null, dislike === 1 ? user_id : null], (err) => {
-            if (err) {
-              console.error('Error creating like or dislike:', err);
-              return res.status(500).json({ message: 'Internal Server Error.' });
-            }
-            res.status(200).json({ message: like === 1 ? 'Like added successfully.' : 'Dislike added successfully.' });
-          });
-        } else {
-          res.status(400).json({ message: 'Invalid request.' });
-        }
+        db.query('INSERT INTO likes (likes, post_id, user_id) VALUES (?, ?, ?)', [1, post_id, user_id], (err) => {
+          if (err) {
+            console.error('Error adding like:', err);
+            return res.status(500).json({ message: 'Internal Server Error.' });
+          }
+          res.status(200).json({ message: 'Like added successfully.', action: 'add' });
+        });
       }
     } catch (error) {
-      console.error('Error processing like or dislike:', error);
+      console.error('Error processing like:', error);
       res.status(500).json({ message: 'Internal Server Error.' });
     }
   });
 };
 
-exports.getLikesAndDislikes = (req, res) => {
-  db.query(`SELECT post_id, likes, dislikes, user_id_liked, user_id_disliked FROM likes`, (err, result) => {
+exports.getLikesByPostId = (req, res) => {
+  const postId = req.params.postId;
+
+  db.query('SELECT * FROM likes WHERE post_id = ?', [postId], (err, result) => {
     if (err) {
       return res.status(500).send(err);
     }
