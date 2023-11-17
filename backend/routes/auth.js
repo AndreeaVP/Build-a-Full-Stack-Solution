8a22efd96ec40129f348f69d15d5710ca69c0194
daisy2511 <andreeagatu58@yahoo.com>
2023-11-17 22:28:30 +0000

    route updated to include multer / enable users to upload a profile image on the sign up form

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require('../middleware/multer');
 const authCtrl = require('../controllers/auth');
 
-router.post('/signup', authCtrl.signup);
+router.post('/signup', multer.single('profileImage'), authCtrl.signup);
 router.post('/login', authCtrl.login);
 
 module.exports = router;
