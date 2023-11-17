e26f201e7ae14687fcd9f81bbb24032d497fea2e
daisy2511 <andreeagatu58@yahoo.com>
2023-11-17 00:19:10 +0000

    spinner added / post container template / fetch posts associated / scss

--- frontend/src/views/userprofile-page.vue
+++ frontend/src/views/userprofile-page.vue
@@ -2,58 +2,166 @@
   <div>
     <app-header></app-header>
 
-    <div v-if="successMessage" class="success-message">{{ successMessage }}</div>
-    <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
-
-    <div class="user-profile">
-      <div class="account-image">
-        <font-awesome-icon :icon="['fas', 'user']" class="user-icon" />
+    <div v-if="loading" class="spinner-container">
+      <div class="spinner">
+        <font-awesome-icon icon="spinner" class="fa-spin fa-3x" />
       </div>
+    </div>
 
-      <div class="profile-info">
-        <p>{{ user.firstname }} {{ user.lastname }}</p>
-        <p class="email">{{ user.email }}</p>
-      </div>
+    <div v-else>
+      <div v-if="successMessage" class="success-message">{{ successMessage }}</div>
+      <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
 
-      <font-awesome-icon @click="toggleSettings" :icon="['fas', 'sliders']" class="icon-settings" v-if="!showSettings" />
-
-      <!-- Settings options -->
-      <div v-if="showSettings" class="settings-container">
-        <div class="close-button-container">
-          <font-awesome-icon :icon="['fas', 'times']" class="icon-close" @click="closeSettings" />
+      <div class="user-myprofile">
+        <div class="account-image">
+          <font-awesome-icon :icon="['fas', 'user']" class="user-icon" />
         </div>
 
-        <div class="profile-image-section">
-          <h3>{{ user.image_url ? 'Update Image' : 'Upload an Image' }}</h3>
-          <img :src="user.image_url" alt="Profile Image">
-          <div class="change-image-button">
-            <button>{{ user.image_url ? 'Change Your Image' : 'Upload an Image' }}</button>
+        <div class="profile-info">
+          <p>{{ user.firstname }} {{ user.lastname }}</p>
+          <p class="email">{{ user.email }}</p>
+        </div>
+
+        <font-awesome-icon @click="toggleSettings" :icon="['fas', 'sliders']" class="icon-settings" v-if="!showSettings" />
+
+        <!-- Settings options -->
+        <div v-if="showSettings" class="settings-container">
+          <div class="close-button-container">
+            <font-awesome-icon :icon="['fas', 'times']" class="icon-close" @click="closeSettings" />
           </div>
-        </div>
 
+          <div class="profile-image-section">
+            <h3>{{ user.image_url ? 'Update Image' : 'Upload an Image' }}</h3>
+            <img :src="user.image_url" alt="Profile Image">
+            <div class="change-image-button">
+              <button>{{ user.image_url ? 'Change Your Image' : 'Upload an Image' }}</button>
+            </div>
+          </div>
 
-        <hr class="separator">
+          <hr class="separator">
         
-        <div class="change-password-section">
-          <h3>Change Password</h3>
-          <div class="new-password-container">
-            <label for="newPassword">Your New Password</label>
-            <input type="password" id="newPassword" v-model="newPassword">
+          <div class="change-password-section">
+            <h3>Change Password</h3>
+            <div class="new-password-container">
+              <label for="newPassword">Your New Password</label>
+              <input type="password" id="newPassword" v-model="newPassword">
+            </div>
+            <div class="submit-button-container">
+              <button @click="changePassword">Submit</button>
+            </div>
           </div>
-          <div class="submit-button-container">
-            <button @click="changePassword">Submit</button>
+
+          <hr class="separator">
+
+          <div class="delete-account-section">
+            <h3>Delete Account</h3>
+            <div class="delete-button-container">
+              <button @click="deleteAccount">Delete Account</button>
+            </div>
           </div>
         </div>
+      </div>  
 
-        <hr class="separator">
+      <!-- User's Posts -->
+      <div v-if="userPosts.length > 0">
 
-        <div class="delete-account-section">
-          <h3>Delete Account</h3>
-          <div class="delete-button-container">
-            <button @click="deleteAccount">Delete Account</button>
+        <div v-for="post in userPosts" :key="post.post_id" class="post-container">
+          <div class="post-container-border">
+            <div class="user-section">
+              <font-awesome-icon :icon="['fas', 'user']" class="user-icon" />
+              <div class="user-details">
+                <div class="user-name">{{ user.firstname }} {{ user.lastname }}</div>
+                <div class="post-date">{{ formatDate(post.created_at) }}</div>
+              </div>
+            </div>
+
+            <div class="post-options">
+              <div v-if="post.showOptions" class="edit-option" @click="toggleEditMode(post)">
+                <div class="update-post-content">
+                  <font-awesome-icon :icon="['fas', 'pencil-alt']" class="update-post-icon" />
+                  <span class="edit-post-text">Edit</span>
+                </div>
+              </div>
+
+              <div v-if="post.showOptions" class="delete-option" @click="confirmDeletePost(post)">
+                <div class="delete-post-content">
+                  <font-awesome-icon :icon="['fas', 'trash']" class="delete-post-icon" />
+                  <span class="delete-post-text">Delete</span>
+                </div>
+              </div>
+
+              <font-awesome-icon v-if="post.showOptions" icon="ellipsis-h" class="ellipsis-icon" @click="toggleOptions(post)"/>
+            </div>
+            <div>
+              <font-awesome-icon v-if="editingPostId === post.post_id" :icon="['fas', 'check']" class="save-post-icon" @click="updatePostText(post)" />
+            </div>
+
+            <div class="post-text" v-if="editingPostId !== post.post_id">
+              {{ post.textual_post }}
+            </div>
+
+            <textarea v-else v-model="newTextualPost" class="update-post-input"></textarea>
+
+            <img v-if="post.image_url" crossorigin="anonymous" :src="post.image_url" alt="Posted Image" class="post-image" />
+
+            <!-- Like and Comment Section -->
+            <div class="like-comment-section">
+              <div class="like-section">
+                <div @click="handleLikeAction(post)" class="like-icon" :class="{ 'liked': post.userLiked }">
+                  <font-awesome-icon :icon="['fas', 'thumbs-up']" class="icon" />
+                  <span class="like-count">{{ post.totalLikes }}</span>
+                 <span v-if="post.userLiked" class="like-text">Liked</span>
+                </div>
+              </div>
+
+              <div class="comment-section">
+                <div @click="toggleCommentInput(post)" class="comment-icon">
+                  <font-awesome-icon :icon="['fas', 'comment']" class="icon" />
+                  <span class="comment-count">{{ post.totalComments }}</span>
+                </div>
+              </div>
+            </div>   
+
+            <!-- Comment Input -->
+            <div class="comment-input" v-if="post.showCommentInput">
+              <input type="text" class="comment-input-text" v-model="post.newComment" @keyup.enter="addComment(post)" placeholder="  Add a comment" />
+              <font-awesome-icon @click="addComment(post)" :icon="['fas', 'paper-plane']" class="save-comment" />
+            </div>
+
+            <!-- Comments Section -->
+            <div class="post-comments-section">
+              <div class="comment" v-for="comment in post.comments" :key="comment.comment_id">
+                <div class="comment-details-container">
+                  <font-awesome-icon :icon="['fas', 'user']" class="comment-user-icon" />
+                  <div class="comment-details">
+                    <span class="comment-user-name">{{ comment.firstname }} {{ comment.lastname }}</span>
+                    <p class="comment-posted-date">Posted on {{ formatDate(comment.created_at) }}</p>
+                  </div>
+                </div>
+
+                <div class="comment-actions">
+                  <font-awesome-icon v-if="!comment.showOptions" icon="ellipsis-h" class="ellipsis-icon" @click="toggleOptionsComment(comment)" />
+                  <div class="comment-actions-container" v-if="comment.showOptions">
+                    <font-awesome-icon :icon="['fas', 'pencil-alt']" class="comment-edit-icon" @click="editComment(comment)" />
+                    <font-awesome-icon :icon="['fas', 'trash']" class="comment-delete-icon" @click="confirmDeleteComment(comment)" />
+                  </div>
+                </div>
+
+                <div v-if="editingCommentId === comment.comment_id">
+                  <input type="text" class="edit-comment-input" v-model="editedComment" />
+                  <font-awesome-icon class="save-edit-comment" @click="saveEditedComment(comment)" :icon="['fas', 'check']"/>
+                </div>
+
+                <div class="comment-text">{{ comment.comment }}</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      <div v-else>
+        <p>No posts available for this user.</p>
+      </div> 
     </div>
   </div>
 </template>
@@ -61,6 +169,7 @@
 <script>
 import AppHeader from "@/components/app-header.vue";
 import axios from "axios";
+import * as postUtils from "@/utils/postUtils";
 
 export default {
   components: {
@@ -71,15 +180,18 @@ export default {
       user: {
         image_url: null,
       },
+      loading: true,
+      userPosts: [],
+      comments: [],
       oldPassword: "",
       newPassword: "",
       showSettings: false,
       successMessage: '',
       errorMessage: '',
+      editingPostId: null,
+      editingCommentId: null,
+      editedComment: "",
     };
-  },
-  computed: {
-
   },
   methods: {
     toggleSettings() {
@@ -92,6 +204,81 @@ export default {
 
     changeProfileImage() {
       // change the profile image
+    },
+
+    editComment(comment) {
+      this.editingCommentId = comment.comment_id;
+      this.editedComment = comment.comment;     
+    },
+
+    toggleOptionsComment(comment) {
+      comment.showOptions = !comment.showOptions;
+    },
+
+    formatDate(dateTimeString) {
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      return new Date(dateTimeString).toLocaleDateString(undefined, options);
+    },
+
+    async fetchUserPosts(userId) {
+      try {
+        const token = localStorage.getItem('token');
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const response = await axios.get(`/api/posts/${userId}`, config);
+
+        if (response.status === 200) {
+          if (response.data.posts.length > 0) {
+            this.userPosts = response.data.posts.map(post => ({
+              ...post,
+              comments: [], 
+              likes: 0,  
+            }));
+
+            await Promise.all(this.userPosts.map(async post => {
+              await this.fetchComments(post);
+              await this.fetchLikes(post);
+            }));
+          } else if (response.status === 404) {
+            this.userPosts = [];
+            this.errorMessage = 'No posts available for this user.';
+          }
+        } else {
+            console.error('Failed to fetch user posts. Please try again.');
+            this.errorMessage = 'Failed to fetch user posts. Please try again.';
+        }
+      } catch (error) {
+          console.error('Error fetching user posts:', error);
+          this.errorMessage = 'Error fetching user posts. Please try again.';
+      }
+    },
+
+    async addComment(post) {
+      await postUtils.addComment.call(this, post);
+    },
+
+    async toggleCommentInput(post) {
+      await postUtils.toggleCommentInput.call(this, post);
+    },
+
+    async fetchComments(post) {
+      await postUtils.fetchComments.call(this, post);
+    },
+    
+    async saveEditedComment(comment) {
+      await postUtils.saveEditedComment.call(this, comment);
+    },
+
+    async handleLikeAction(post, action) {
+      await postUtils.handleLikeAction.call(this, post, action);
+    },
+
+    async fetchLikes(post) {
+      await postUtils.fetchLikes.call(this, post);
     },
 
     async changePassword() {
@@ -107,14 +294,14 @@ export default {
           },
         });
 
-      if (response.status === 200) {
-        this.successMessage = 'Password changed successfully.';
-        this.errorMessage = '';
-        this.newPassword = '';
-        setTimeout(() => {
-          this.successMessage = '';
-        }, 1500);
-      }
+        if (response.status === 200) {
+          this.successMessage = 'Password changed successfully.';
+          this.errorMessage = '';
+          this.newPassword = '';
+          setTimeout(() => {
+            this.successMessage = '';
+          }, 1500);
+        }
       } catch (error) {
         console.error('Error changing password:', error);
         this.errorMessage = 'Failed to change password. Please try again.';
@@ -122,44 +309,42 @@ export default {
     },
 
     async deleteAccount() {
-  const token = localStorage.getItem('token');
+      const token = localStorage.getItem('token');
+      const confirmed = confirm("Are you sure you want to delete your account? This action cannot be undone.");
+        if (!confirmed) {
+      return;
+      }
 
-  const confirmed = confirm("Are you sure you want to delete your account? This action cannot be undone.");
+      try {
+        const userId = this.$store.state.user.user_id;
+        const response = await axios.delete(`/api/user/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-  if (!confirmed) {
-    return;
-  }
-
-  try {
-    const userId = this.$store.state.user.user_id;
-
-    const response = await axios.delete(`/api/user/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (response.status === 200) {
-      this.successMessage = response.data.message;
-      this.errorMessage = '';
-      setTimeout(() => {
-        this.successMessage = '';
-        this.$router.push({ name: 'login' });
-      }, 1500);
-    } else {
-      this.errorMessage = 'Error deleting user account. Please try again later.';
-    }
-  } catch (error) {
-    console.error('Error deleting user account:', error);
-  }
-},
+        if (response.status === 200) {
+          this.successMessage = response.data.message;
+          this.errorMessage = '';
+          setTimeout(() => {
+            this.successMessage = '';
+            this.$router.push({ name: 'login' });
+          }, 1500);
+        } else {
+          this.errorMessage = 'Error deleting user account. Please try again later.';
+        }
+      } catch (error) {
+        console.error('Error deleting user account:', error);
+      }
+    },
   },
 
   async created() {
+    this.loading = true;
     let userId = this.$route.query.user_id || this.$store.state.user.userId;
-    if (!userId) {
-      userId = this.$store.state.user.user_id;
-    }
+      if (!userId) {
+        userId = this.$store.state.user.user_id;
+      }
 
     const token = localStorage.getItem('token');
       if (!token) {
@@ -173,16 +358,55 @@ export default {
           Authorization: `Bearer ${token}`,
         },
       });
-      this.user = response.data.user;
+        this.user = response.data.user;
+        await this.fetchUserPosts(userId);
     } catch (error) {
       console.error("Error fetching user data:", error);
-    }
+    } finally {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1300);
+  }
   }
 };
 </script>
 
-<style scoped>
-.user-profile {
+<style lang="scss">
+@import '@/styles/main';
+
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.spinner {
+  animation: spin 1.3s linear infinite, changeColor 5s infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes changeColor {
+  0% {
+    color: blue;
+  }
+
+  50% {
+    color: red;
+  }
+  75% {
+    color: #8B008B;
+  }
+}
+.user-myprofile {
   position: relative;
   display: flex;
   align-items: center;
@@ -195,29 +419,6 @@ export default {
   margin: 110px auto 30px auto;
   background: linear-gradient(180deg,  rgb(169, 181, 251),#fbedc6);
   color: #000;
-}
-.account-image {
-  margin-left: 40px;
-
-}
-  
-.user-icon {
-  color: #333;  
-  font-size: 40px;
-  padding: 10px;
-  border-radius: 50%;
-  margin-right: 30px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3); 
-}
-
-.profile-info {
-  display: flex;
-  flex-direction: column;
-  margin-right: 20px;
-}
-
-.email {
-  margin-top: 10px;
 }
 
 .icon-settings {
@@ -290,30 +491,6 @@ export default {
 .separator {
   width: 90%;
   border: 1px solid #eb0db3;
-}
-
-.success-message {
-  color: white;
-  background-color: #55eb5a;
-  padding: 8px;
-  border-radius: 5px; 
-  position: fixed;
-  top: 70px;
-  left: 0;
-  right: 0;
-  z-index: 1;
-}
-
-.error-message {
-  color: white;
-  background-color: red;
-  padding: 8px;
-  border-radius: 5px; 
-  position: fixed;
-  top: 70px;
-  left: 0;
-  right: 0;
-  z-index: 1;
 }
 
 @media (max-width: 768px) {
